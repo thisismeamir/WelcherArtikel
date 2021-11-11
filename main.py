8f5a2f3ebd81e73ea6edfd1346bdb661a4a2d941
Amir Hosein <thisismeamir@outlook.com>
2021-11-11 18:46:50 +0330

    wordClass and artikel Requester

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
+import requestsToWeb as rtw
+import os
+import telebot
 
+
+
+# API_KEY - Creating Bot
+API_KEY = os.getenv('API_KEY')
+bot = telebot.TeleBot(API_KEY)
+
+# Bot
+@bot.message_handler(command=['Greet'])
+def greet(message):
+    bot.reply_to(message, "Hey! How it going")
+
+bot.polling()
